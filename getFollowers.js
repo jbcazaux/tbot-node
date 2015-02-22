93507d450fb96a13f6699c895dff3efb7c2d9096
jb cazaux <jbcazaux@gmail.com>
2015-02-22 22:59:30 +0100

    distribute get followers requests over time

--- getFollowers.js
+++ getFollowers.js
@@ -2,42 +2,47 @@ var request = require('./requester');
 var ToMongo = require('./toMongo');
 var ToCursoredList = require('./toCursoredList');
 var Q = require('q');
+var TimeRunner = require('./timeRunner');
+
 
 var options = {db: 'mongodb://localhost:27017/twitter', collection: 'profiles'};
 var toMongo = ToMongo(options);
+var timeRunner = TimeRunner(15, 15 * 60 * 1000);
 
-
-function getFollowers(twitterId, cursor) {
-    console.log('will request with cursor ', cursor , 'and id', twitterId) ;
-    return request('GET', 'https://api.twitter.com/1.1/followers/ids.json?cursor=' + cursor + '&user_id=' + twitterId + '&count=50');
+function getFollowersRequest(twitterId, cursor) {
+    console.log('will request with cursor ', cursor, 'and id', twitterId);
+    return request('GET', 'https://api.twitter.com/1.1/followers/ids.json?cursor=' + cursor + '&user_id=' + twitterId + '&count=5000');
 }
 
 var ids = [];
-function generateStream(followedId, cursor, cb) {
-    var data;
-    var s = getFollowers(followedId, cursor).pipe(ToCursoredList());
 
-    s.on('data', function(chunk) {
-        data = chunk;
-        ids = ids.concat(data.ids);
-    });
-    s.on('end', function() {
-        var promises = [];
-        data.ids.forEach(function(followerId){
-            promises.push(toMongo.addFollowed(followerId, followedId));
+function getFollowers(followedId, cursor, cb) {
+    return function () {
+        var data;
+        var s = getFollowersRequest(followedId, cursor).pipe(ToCursoredList());
+
+        s.on('data', function (chunk) {
+            data = chunk;
+            ids = ids.concat(data.ids);
         });
-        Q.allSettled(promises).then(function(){
-            if (data.next_cursor > 0) {
-                generateStream(followedId, data.next_cursor, cb);
-            }else {
-                cb(ids);
-            }
+        s.on('end', function () {
+            var promises = [];
+            data.ids.forEach(function (followerId) {
+                promises.push(toMongo.addFollowed(followerId, followedId));
+            });
+            Q.allSettled(promises).then(function () {
+                if (data.next_cursor > 0) {
+                    timeRunner.run(getFollowers(followedId, data.next_cursor, cb));
+                } else {
+                    cb(ids);
+                }
+            });
         });
-    });
+    }
 }
 
-generateStream(52667847, -1, function(ids){
+timeRunner.run(getFollowers(52667847, -1, function (ids) {
     console.log('final: ', ids.length);
-});
+}));
 
-module.exports = generateStream;
+//module.exports = getFollowers;
