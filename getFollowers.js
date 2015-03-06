40f23a00868d7c457899827022492dfdf2c74451
jbcazaux <jbcazaux@gmail.com>
2015-03-06 17:21:09 +0100

    clean memory leaks

--- getFollowers.js
+++ getFollowers.js
@@ -4,7 +4,6 @@ var ToCursoredList = require('./toCursoredList');
 var Q = require('q');
 var TimeRunner = require('./timeRunner');
 
-
 var options = {db: 'mongodb://localhost:27017/twitter', collection: 'profiles'};
 var toMongo = ToMongo(options);
 var timeRunner = TimeRunner(15, 15 * 60 * 1000);
@@ -14,13 +13,13 @@ function getFollowersRequest(twitterId, cursor) {
     return request('GET', 'https://api.twitter.com/1.1/followers/ids.json?cursor=' + cursor + '&user_id=' + twitterId + '&count=5000');
 }
 
-var ids = [];
-
 function getFollowers(followedId, cursor, cb) {
+
     return function () {
+        var ids = [];
         var data;
         var s = getFollowersRequest(followedId, cursor).pipe(ToCursoredList());
-
+        var hd = new memwatch.HeapDiff();
         s.on('data', function (chunk) {
             data = chunk;
             ids = ids.concat(data.ids);
@@ -31,18 +30,21 @@ function getFollowers(followedId, cursor, cb) {
                 promises.push(toMongo.addFollowed(followerId, followedId));
             });
             Q.allSettled(promises).then(function () {
+                ids.length = 0;
+                s = null;
                 if (data.next_cursor > 0) {
                     timeRunner.run(getFollowers(followedId, data.next_cursor, cb));
                 } else {
-                    cb(ids);
+                    cb();
                 }
             });
         });
     }
 }
 
-timeRunner.run(getFollowers(52667847, -1, function (ids) {
-    console.log('final: ', ids.length);
+var followersOf = 18814998;
+timeRunner.run(getFollowers(followersOf, -1, function () {
+    console.log('done getting followings of', followersOf);
 }));
 
 //module.exports = getFollowers;
